8650b82c8965f50d94ec126c01c2abeac6b89825
Rafael <rafaelbarrosalmeida770@gmail.com>
2026-05-28 10:35:17 -0300

    Decimo Segundo Commit

--- Escola de Idiomas/obj/Debug/net8.0-windows/Escola de Idiomas.AssemblyInfo.cs
+++ Escola de Idiomas/obj/Debug/net8.0-windows/Escola de Idiomas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Escola de Idiomas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e154dd73476b0b6a72d98a7f9ae1ac6dae5bbb36")]
 [assembly: System.Reflection.AssemblyProductAttribute("Escola de Idiomas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Escola de Idiomas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
